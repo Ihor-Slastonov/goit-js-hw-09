a3c6978a3a55a4294f8136fe04f133aed3c5fef1
Ihor-Slastonov <104766756+Ihor-Slastonov@users.noreply.github.com>
2022-10-05 23:52:06 +0300

    update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,73 @@
 const body = document.querySelector('body');
-const buttons = document.querySelector('.buttons');
-const startBtn = buttons.firstElementChild;
-const stopBtn = buttons.lastElementChild;
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
-buttons.addEventListener('click', onButtonsClick);
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+let currentColorId = null;
+stopBtn.disabled = true;
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
-let randomColorId = null;
+function onStartBtn() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
 
-function onButtonsClick(e) {
-    if (e.target === startBtn) {
-        startBtn.disabled = true;
-        const randomColors = setInterval(() => {
-            body.style.backgroundColor = getRandomHexColor();
-            return randomColorId = randomColors;
-        }, 1000);
-    }
-    if (e.target === stopBtn) {
-        clearInterval(randomColorId);
-        startBtn.disabled = false;
-    }
-};
+    const randomColor = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        currentColorId = randomColor;
+    }, 1000)
+}
+
+function onStopBtn() {
+    clearInterval(currentColorId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const buttons = document.querySelector('.buttons');
+// const startBtn = buttons.firstElementChild;
+// const stopBtn = buttons.lastElementChild;
+
+// buttons.addEventListener('click', onButtonsClick);
+
+
+
+// let randomColorId = null;
+
+// function onButtonsClick(e) {
+//     if (e.target === startBtn) {
+//         startBtn.disabled = true;
+//         const randomColors = setInterval(() => {
+//             body.style.backgroundColor = getRandomHexColor();
+//             return randomColorId = randomColors;
+//         }, 1000);
+//     }
+//     if (e.target === stopBtn) {
+//         clearInterval(randomColorId);
+//         startBtn.disabled = false;
+//     }
+// };
