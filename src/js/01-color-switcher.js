faf990e591ea47e54be043531cf19381db00aade
Ihor-Slastonov <104766756+Ihor-Slastonov@users.noreply.github.com>
2022-10-05 22:06:13 +0300

    first done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body');
+const buttons = document.querySelector('.buttons');
+const startBtn = buttons.firstElementChild;
+const stopBtn = buttons.lastElementChild;
 
+buttons.addEventListener('click', onButtonsClick);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+let randomColorId = null;
+
+function onButtonsClick(e) {
+    if (e.target === startBtn) {
+        startBtn.disabled = true;
+        const randomColors = setInterval(() => {
+            body.style.backgroundColor = getRandomHexColor();
+            return randomColorId = randomColors;
+        }, 1000);
+    }
+    if (e.target === stopBtn) {
+        clearInterval(randomColorId);
+        startBtn.disabled = false;
+    }
+};
