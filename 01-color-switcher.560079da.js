f52d4d00e876c2a61818597a75066b0a31d5984e
Ihor-Slastonov <104766756+Ihor-Slastonov@users.noreply.github.com>
2022-10-07 14:40:41 +0000

    Deploying to gh-pages from @ Ihor-Slastonov/goit-js-hw-09@003d9b938f2f25f7c831078db9cf05804bacd815 🚀

---
+++ 01-color-switcher.560079da.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1;const a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,d=a}),1e3)})),t.addEventListener("click",(function(){clearInterval(d),t.disabled=!0,e.disabled=!1}));let d=null;t.disabled=!0;
+//# sourceMappingURL=01-color-switcher.560079da.js.map
